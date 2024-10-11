ca5fe3a919b12118a116d988a139862f8665ef35
Iqra327 <iqrarasheed219@gmail.com>
2024-10-11 23:02:38 +0500

    initial-commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400'>
+
+    </section>
+  )
+}
+
+export default Hero

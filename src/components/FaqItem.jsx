32dddcb1560224aaeb5d306418821d894942e2ba
Iqra327 <iqrarasheed219@gmail.com>
2024-10-12 23:04:39 +0500

    completed

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx'
 import React, { useState } from 'react'
-import { SlideDown } from 'react-slidedown';
 import 'react-slidedown/lib/slidedown.css'
 
 const FaqItem = ({item, index}) => {
@@ -28,9 +27,9 @@ const FaqItem = ({item, index}) => {
         </div>
       </div>
 
-      <SlideDown>
+      <div>
         {activeId === item.id && <div className='body-3 px-7 py-3.5'>{item.answer}</div>}
-      </SlideDown>
+      </div>
 
       <div className={clsx('g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute', active && 'opacity-100')}>
         <div className='g4 absolute inset-0.5 -z-1 rounded-3xl'/>
